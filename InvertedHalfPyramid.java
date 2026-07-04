437a511684b417c1f149e0b8cb9a0b0f6e81981e
tanishkasoni30 <tanishkasoni9826@gmail.com>
2026-07-04 18:01:17 +0530

    Inverted Half Pyramid

---
+++ InvertedHalfPyramid.java
@@ -0,0 +1,15 @@
+// ***** 
+// ****
+// ***
+// **
+// *
+public class InvertedHalfPyramid {
+	public static void main (String []args){
+		for(int i = 1 ; i <=5; i++){
+			for (int j = 0 ; j<=5-i; j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
