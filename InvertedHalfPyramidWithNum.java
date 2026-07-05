179ca6fc67ee2978c5996c1f2487fc8688931d94
tanishkasoni30 <tanishkasoni9826@gmail.com>
2026-07-05 20:07:59 +0530

    InvertedHalfPyramidWithNum

---
+++ InvertedHalfPyramidWithNum.java
@@ -0,0 +1,19 @@
+// 12345
+// 1234
+// 123
+// 12
+// 1
+
+public class InvertedHalfPyramidWithNum {
+	public static void main(String[] args){
+		for(int i = 1 ; i<=5; i++){
+			for(int j = 1;j<=5-i+1;j++){
+System.out.print(j+" ");
+
+			}
+			System.out.println();
+		}
+
+	}
+	
+}
